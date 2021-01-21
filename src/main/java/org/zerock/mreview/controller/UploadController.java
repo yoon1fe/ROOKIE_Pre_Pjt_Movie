9e087ad1fd9b5be2718d8bcae019588989b8f57b
yoon1fe <yoon1fe@naver.com>
2021-01-21 21:16:23 +0900

    [#3] 이미지 파일 업로드 및 삭제 기능 연습

--- src/main/java/org/zerock/mreview/controller/UploadController.java
+++ src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -11,9 +11,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.zerock.mreview.dto.UploadResultDTO;
+import net.coobird.thumbnailator.Thumbnailator;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -62,7 +64,18 @@ public class UploadController {
             Path savePath = Paths.get(saveName);
 
             try {
+                // 원본 파일 저장
                 uploadFile.transferTo(savePath);
+
+                // 섬네일 생성
+                String thumbnailSaveName = uploadPath + File.separator + folderPath + File.separator
+                        + "s_" + uuid + "_" + fileName;
+
+                // 섬네일 파일 이름은 중간에 s_ 로 시작하도록
+                File thumbnailFile = new File(thumbnailSaveName);
+
+                // 섬네일 생성
+                Thumbnailator.createThumbnail(savePath.toFile(), thumbnailFile, 100, 100);
                 resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
             } catch (IOException e) {
                 e.printStackTrace();
@@ -113,5 +126,26 @@ public class UploadController {
         }
 
         return result;
+    }
+
+    @PostMapping("/removeFile")
+    public ResponseEntity<Boolean> removeFile(String fileName) {
+
+        String srcFileName = null;
+        try{
+            srcFileName = URLDecoder.decode(fileName, "UTF-8");
+            File file = new File(uploadPath + File.separator + srcFileName);
+            boolean result = file.delete();
+
+            File thumbnail = new File(file.getParent(), "s_" + file.getName());
+
+            result = thumbnail.delete();
+
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        }catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 }
